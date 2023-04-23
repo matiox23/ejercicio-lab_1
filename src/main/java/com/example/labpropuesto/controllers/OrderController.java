997bfc9b7c675836149114ce6cbba929902d3c90
matiox23 <matias_ramirez@hotmail.com>
2023-04-22 22:37:47 -0500

    Creación del controlador OrderController: • "feat: Crear controlador OrderController para manejar las solicitudes HTTP."

--- src/main/java/com/example/labpropuesto/controllers/OrderController.java
+++ src/main/java/com/example/labpropuesto/controllers/OrderController.java
@@ -1,27 +1,38 @@
 package com.example.labpropuesto.controllers;
 
-
 import com.example.labpropuesto.models.Food;
+import com.example.labpropuesto.models.Order;
 import com.example.labpropuesto.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
 public class OrderController {
+
     @Autowired
     private OrderService orderService;
 
-    @GetMapping("/menu")
-    public List<Food> getMenu() {
-        return orderService.getMenu();
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrder(@PathVariable Long orderId) {
+
+        Order order = orderService.getOrderById(orderId);
+
+        return ResponseEntity.ok(order);
+    }
+
+    @PostMapping("/foods")
+    public Food createFood(@RequestBody Food food) {
+        return food;
     }
 
 
 
+
+
 }
-
-
-
