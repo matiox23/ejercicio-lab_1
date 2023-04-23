0dd0f24c50a0dd6514aa428e0391a2bcf2c38f5b
matiox23 <matias_ramirez@hotmail.com>
2023-04-22 21:03:38 -0500

    Creación de la clase Food:  "Crear clase Food con los atributos name, description y price."

---
+++ src/main/java/com/example/labpropuesto/models/Food.java
@@ -0,0 +1,38 @@
+package com.example.labpropuesto.models;
+
+public class Food {
+    private String name;
+    private String description;
+    private Double price;
+    public Food(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+}
